e5ec58ec14ee65ada3a8c0d0bf98fd48bd04d8ad
Heejay Kong <heejay.dev@gmail.com>
2021-02-13 19:42:28 +0900

    Update 2110.py

--- 2110.py
+++ 2110.py
@@ -22,7 +22,7 @@ answer = 0
 while shortest <= longest:
   midDistance = (shortest+longest)//2
   wifiCount = routerCount(midDistance)
-  if wifiCount >= fixedWifiCount:  # 이 부분 이해 안 가는 건 https://blog.naver.com/pjok1122/221652210187 참고("잘 생각해보면, 공유기의 갯수가 C와 일치하더라도, 공유기의 '간격(mid)'을 좀 더 크게 설치해볼 필요성이 있다.")
+  if wifiCount >= fixedWifiCount:  # 공유기를 목표치 이상으로 설치할 수 있다면, 정답이 될 수 있음. 이 부분 이해 안 가는 건 https://blog.naver.com/pjok1122/221652210187 참고("잘 생각해보면, 공유기의 갯수가 C와 일치하더라도, 공유기의 '간격(mid)'을 좀 더 크게 설치해볼 필요성이 있다.")
     # 거리를 넓혀야 함
     shortest = midDistance + 1
     answer = midDistance
