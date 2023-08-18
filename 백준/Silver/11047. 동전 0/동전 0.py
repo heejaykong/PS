8460fefed687ae5fb5900ba87ff290679758487e
Heejay Kong <heejay.dev@gmail.com>
2023-08-18 11:50:30 +0900

    [Silver IV] Title: 동전 0, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+stack = []
+for i in range(n):
+  stack.append(int(input()))
+ans = 0
+for i in range(n-1, -1, -1):
+  if k // stack[i] > 0:
+    ans += k // stack[i]
+    k %= stack[i]
+print(ans)
